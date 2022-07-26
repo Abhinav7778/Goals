8ef3514da551d29f55059df94981e244580a3421
Abhinav Shukla <abhinavshukla@Abhinavs-MacBook-Air.local>
2022-07-26 14:21:39 -0400

    Routes and Controller setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,18 @@
-console.log("Hello Goal setter")
+const express = require('express')
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errorMiddlewareHandler')
+
+const port = process.env.PORT || 5000
+
+const app = express()
+
+//express json and express url encoded added so that we can send the body data and it can be processed by the BE, these two lines belong to the Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
+
